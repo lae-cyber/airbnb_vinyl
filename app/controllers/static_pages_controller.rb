3e6827686da6535324b96f3ea3ed815e100f370e
laetitia steybe <ls178456@protonmail.com>
2024-02-17 11:54:07 +0100

    home path fixed with TA

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,13 @@
 class StaticPagesController < ApplicationController
+  skip_before_action :authenticate_user!, only: :home
+
   def home
+    @vinyls = Vinyl.all
+    if params[:query].present?
+      @query = params[:query]
+      @vinyls = Vinyl.where("title LIKE ?", "%#{params[:query]}%")
+    else
+      @vinyls = Vinyl.all
+    end
   end
 end
--- app/controllers/vinyls_controller.rb
+++ app/controllers/vinyls_controller.rb
@@ -1,4 +1,5 @@
 class VinylsController < ApplicationController
+  skip_before_action :authenticate_user!, only: :index
   before_action :set_vinyl, only: [:show, :edit, :update, :destroy]
 
   def index
