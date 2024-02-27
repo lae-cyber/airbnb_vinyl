e8b9125a693181dfbf1ea68e491e3d26b3d022ba
laetitia steybe <ls178456@protonmail.com>
2024-02-27 12:16:29 +0100

    map box can be used now

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_24_112422) do
+ActiveRecord::Schema[7.1].define(version: 2024_02_27_100317) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -57,6 +57,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_24_112422) do
     t.string "card_cvc"
     t.string "firstname"
     t.string "lastname"
+    t.float "latitude"
+    t.float "longitude"
     t.index ["user_id"], name: "index_bookings_on_user_id"
     t.index ["vinyl_id"], name: "index_bookings_on_vinyl_id"
   end
